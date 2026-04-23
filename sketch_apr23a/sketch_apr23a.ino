6942ccdbd975f6511197a9c061ad649cf23e850c
Anlimah <y.m.ratty7@gmail.com>
2026-04-23 10:59:30 +0000

    changed light flow

--- sketch_apr23a/sketch_apr23a.ino
+++ sketch_apr23a/sketch_apr23a.ino
@@ -1,23 +1,33 @@
-int PinA=2;
-int PinB=3;
-int PinC=4;
+int PinA = 2;
+int PinB = 3;
+int PinC = 4;
 
-void setup() {
-  pinMode(PinA, OUTPUT);
-  pinMode(PinB, OUTPUT);
-  pinMode(PinC, OUTPUT);
+void setup()
+{
+    pinMode(PinA, OUTPUT);
+    pinMode(PinB, OUTPUT);
+    pinMode(PinC, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(PinA, HIGH); 
-  delay(2000);
-  
-  digitalWrite(PinA, LOW); 
-  digitalWrite(PinB, HIGH);
-  delay(2000);
-  digitalWrite(PinB, LOW);
-  digitalWrite(PinC, HIGH); 
-  delay(2000);
-  digitalWrite(PinC, LOW); 
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    digitalWrite(PinA, HIGH);
+    delay(2000);
+    digitalWrite(PinA, LOW);
+    digitalWrite(PinB, HIGH);
+    delay(2000);
+    digitalWrite(PinB, LOW);
+    delay(100);
+    digitalWrite(PinB, HIGH);
+    delay(100);
+    digitalWrite(PinB, LOW);
+    delay(100);
+    digitalWrite(PinB, HIGH);
+    delay(100);
+    digitalWrite(PinB, LOW);
+    delay(100);
+    digitalWrite(PinC, HIGH);
+    delay(5000);
+    digitalWrite(PinC, LOW);
 }
